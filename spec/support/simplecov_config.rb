b489441e6bdc638c09b78dad8161c0c191934fbe
George Mihai Grigore <grigore.george.mihaii@gmail.com>
2026-08-04 11:30:53 +0300

    Bump gems/ci

--- spec/support/simplecov_config.rb
+++ spec/support/simplecov_config.rb
@@ -3,10 +3,5 @@
 require "simplecov"
 
 SimpleCov.start "rails" do
-  add_filter "/bin/"
-  add_filter "/db/"
-  add_filter "/spec/"
-  add_filter "/config/"
-  add_filter "/lib/generators/"
-  add_filter "app/admin"
+  skip "/lib/generators/"
 end
